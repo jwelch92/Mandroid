6efeac6898b52c43d267d9fcf96d52b118969e22
Jared Welch <jwelch92@gmail.com>
2014-08-13 20:39:35 -0700

    Finished basic audio player.

--- HelloMoon/app/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
+++ HelloMoon/app/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
@@ -11,17 +11,38 @@ import android.widget.Button;
  * Created by jared on 8/13/2014.
  */
 public class HelloMoonFragment extends Fragment {
-
+    private AudioPlayer mPlayer = new AudioPlayer();
     private Button mPlayButton;
     private Button mStopButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
+
+
         mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.play(getActivity());
+            }
+        });
+
         mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.stop();
+            }
+        });
 
         return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mPlayer.stop();
     }
 
 }
